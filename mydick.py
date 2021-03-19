e516bde88ee8ad130939c0605e19ed3a3374c833
PxndxDev <80158828+PxndxDev@users.noreply.github.com>
2021-03-19 17:50:39 +0100

    Update mydick.py

--- mydick.py
+++ mydick.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 x1 = [-2, -2, -1, 0, 1, 2, 2]
 y1 = [-1, 5, 6, 6.5, 6, 5, -1]
 
-plt.plot(x1, y1, label="testicules")
+plt.plot(x1, y1, label="queue")
 
 x2 = [-2, -3, -3,     -3,   -2.5,   -1.5, -0.5,   0,     0.5,  1.5, 2.5,  3,    3,     3, 2]
 y2 = [-1, -2, -2.5,   -3.5, -4.5,   -5,   -4.5,   -3.5,  -4.5, -5,  -4.5, -3.5, -2.5, -2, -1]
 
-plt.plot(x2, y2, label="verge")
+plt.plot(x2, y2, label="testicules")
 
 plt.xlabel('dimensions x')
 
